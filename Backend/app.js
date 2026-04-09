f4e74e7ae69ad3e0537693a9565c3bf481ed137e
DarkEgale <darkegale3536@gmail.com>
2026-04-09 09:23:37 -0400

    cors fix

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(cors({
 
-    origin: 'http://localhost:5173', 
+    origin: 'https://www.mdshimulhossen.top', 
 
     credentials: true, 
 
