e7f815596452c4258d912d8e5b3a7ad22ba01946
DarkEgale <darkegale3536@gmail.com>
2026-05-01 23:14:01 +0600

    incress security 3

--- Backend/Router/Public/publicRoute.js
+++ Backend/Router/Public/publicRoute.js
@@ -1,14 +1,26 @@
 import { ProjectsControllers,singleProject } from "../../Controllers/ProjectControlleres.js";
 import { getBlogs, getBlogById } from "../../Controllers/BlogControllers.js";
-import { validateObjectId } from "../../Middleware/middleware.js";
 import Router from 'express';
+import mongoose from 'mongoose';
 
 const router=Router()
 
+const validatePublicObjectId = (req, res, next) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid id',
+        });
+    }
+
+    next();
+};
+
 router.get('/projects',ProjectsControllers)
-router.get('/projects/:id',validateObjectId(),singleProject)
+router.get('/projects/:id',validatePublicObjectId,singleProject)
 router.get('/blogs', getBlogs)
-router.get('/blogs/:id', validateObjectId(), getBlogById)
+router.get('/blogs/:id', validatePublicObjectId, getBlogById)
 
 
 export default router;
